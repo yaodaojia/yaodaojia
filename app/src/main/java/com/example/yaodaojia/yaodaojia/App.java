7d9ed596b1caca8f41c81d47c1884fd83bd06c06
axi0712 <1255645340@qq.com>
2017-08-09 11:00:36 +0800

    加了个判断

--- app/src/main/java/com/example/yaodaojia/yaodaojia/App.java
+++ app/src/main/java/com/example/yaodaojia/yaodaojia/App.java
@@ -1,8 +1,15 @@
 package com.example.yaodaojia.yaodaojia;
 
+import android.app.AlertDialog;
 import android.app.Application;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.example.yaodaojia.yaodaojia.base.BaseActivity;
+import com.example.yaodaojia.yaodaojia.control.activity.MainActivity;
 
 /**
  * Created by axi on 2017/8/8.
@@ -10,4 +17,10 @@ import com.example.yaodaojia.yaodaojia.base.BaseActivity;
 
 public class App extends Application {
     public static BaseActivity activity;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
 }
--- app/src/main/java/com/example/yaodaojia/yaodaojia/base/BaseActivity.java
+++ app/src/main/java/com/example/yaodaojia/yaodaojia/base/BaseActivity.java
@@ -1,11 +1,20 @@
 package com.example.yaodaojia.yaodaojia.base;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.yaodaojia.yaodaojia.App;
+import com.example.yaodaojia.yaodaojia.R;
+import com.example.yaodaojia.yaodaojia.control.activity.MainActivity;
 
 import butterknife.ButterKnife;
 
@@ -18,12 +27,45 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         App.activity = this;
+        getLocation();
         setContentView(getLayout());
         ButterKnife.bind(this);
         initView();
         initListener();
 
     }
+
+    private void getLocation() {
+        PackageManager pm = getPackageManager();
+        boolean flag = (PackageManager.PERMISSION_GRANTED ==
+                pm.checkPermission("android.permission.RECORD_AUDIO", "packageName"));
+        if (flag) {
+            //有这个权限，做相应处理
+            Intent in = new Intent(this, MainActivity.class);
+            startActivity(in);
+        }else {              //没有权限
+            View v = LayoutInflater.from(this).inflate(R.layout.location_dialog,null);
+            final Dialog dia = new AlertDialog.Builder(this).setView(v).create();
+            dia.show();
+            TextView mCancel = v.findViewById(R.id.location_cancel);
+            TextView mSure =  v.findViewById(R.id.location_sure);
+            mCancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dia.dismiss();
+                }
+            });
+            mSure.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dia.dismiss();
+                    //开始定位
+                }
+            });
+
+        }
+    }
+
     //找布局
     public abstract int getLayout();
     //找ID
